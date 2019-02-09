c2c6b42a5602bed660e5a30a54dfe1b93522e586
NikhilRajPandey <nikhilrajpandey1@gmail.com>
2019-02-09 21:06:12 +0530

    Front Page made and layout.html and anoter dashboard.css , style.css added.

--- __init__.py
+++ __init__.py
@@ -2,10 +2,13 @@ from flask import Flask , render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 
 # Main Work
 @app.route('/')
 def home():
     return render_template('index.html')
 
-app.run()
+app.run(debug=True)
