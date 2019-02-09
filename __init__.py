3bdc9bfc4b6a53bd71e7901a447b10a658906895
NikhilRajPandey <nikhilrajpandey1@gmail.com>
2019-02-09 21:21:36 +0530

    Making a json file.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 from flask import Flask , render_template
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+# Loading config.json as params
+with open('config.json','r') as file:
+    params = json.load(file)
 
 # Main Work
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',webname=params['websitename'])
 
 app.run(debug=True)
